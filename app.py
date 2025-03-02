24a373c44d4f6d6d8bcf1b9bcf9b316456a5e640
ashwantmanikoth <ashwanta75@gmail.com>
2025-03-02 17:15:25 -0400

    fix: Title changes minor
    Minor changes in title and description

--- app.py
+++ app.py
@@ -14,21 +14,24 @@ models = get_models_from_properties("models.properties")
 modes = ["RAG Fusion", "Multi Query RAG"]
 
 with gr.Blocks() as ui:
+    gr.Markdown("""#    <p style="text-align:center;">IntelliSearch Engine!</p>
+                 <p style="text-align:center;">An AI-powered search assistant using Retrieval Augmented Generation (RAG).</p>
+    """) 
     with gr.Row():
         with gr.Column(scale=1):
-            gr.Markdown("<br><br><br>")  # Moves sidebar down
+            gr.Markdown("<br>")  # Moves sidebar down
             model_dropdown = gr.Dropdown(choices=models, label="Select a LLM model")
             temp_slider = gr.Slider(0.0, 1.0, step=0.05, value=0.0, label="Adjust the temperature")
             n_results_slider = gr.Slider(3, 20, step=1, value=5, label="Number of Search Results")
             search_type_radio = gr.Radio(choices=["Search", "Chat (Include History)"], label="Search Type")
-            rag_type_dropdown = gr.Dropdown(choices=["RAG Fusion", ""], label="Select type of retrieval")
+            rag_type_dropdown = gr.Dropdown(choices=modes, label="Select type of retrieval")
 
         with gr.Column(scale=3):
             chatbot = gr.ChatInterface(
                 fn=server,  # Dummy function for testing
                 additional_inputs=[model_dropdown, temp_slider, n_results_slider, search_type_radio, rag_type_dropdown],
-                title="AI-powered RAG Web Crawler",
-                description="An AI-powered search assistant using Retrieval Augmented Generation (RAG).",
+                title="",
+                description="",
             )
     
     gr.Markdown("### Powered by Qdrant, Gradio, and LangChain")
